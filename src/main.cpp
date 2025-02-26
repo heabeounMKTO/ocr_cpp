f94a3bafe4d736fa286bb90eb74bed8ef76bfa9b
heabeounMKTO <heabeoun69zin@gmail.com>
2025-02-26 14:12:33 +0700

    letter box

--- src/main.cpp
+++ src/main.cpp
@@ -8,104 +8,19 @@
 #include <torch/script.h>
 #include <vector>
 
-std::vector<Detection> run_inference(YoloModel *detector,
-                                     cv::Mat *input_image) {
-
-  // torch::Device device(torch::kCPU);
-  std::vector<Detection> detections;
-  torch::Tensor input_tensor;
-
-  auto o_width = input_image->cols;
-  auto o_height = input_image->rows;
-
-  int new_w, new_h;
-  float ratio;
-  scale_wh(o_width, o_height, &new_w, &new_h, &ratio);
-  fprintf(stdout, "new_w %d new_h %d\n", new_w, new_h);
-
-  preprocess(input_image, &input_tensor, INFERENCE_SIZE);
-  printf("input_image tensor shape: [");
-  for (int i = 0; i < input_tensor.dim(); i++) {
-    printf("%ld%s", input_tensor.size(i),
-           i < input_tensor.dim() - 1 ? ", " : "");
-  }
-  printf("]\n");
-
-  try {
-    torch::NoGradGuard no_grad;
-    std::vector<torch::jit::IValue> _inputs;
-    _inputs.push_back(input_tensor);
-
-    auto output = detector->model.forward(_inputs).toTensor();
-
-    output = output.to(torch::kCPU);
-
-    /*  [1, 14, 8400]
-     https://github.com/orgs/ultralytics/discussions/17254
-     */
-    int num_detections = output.size(1);
-    int feats_size = output.size(2);
-    int all_classes = num_detections - 5;
-    auto nclasses = feats_size - 5;
-
-    /* fprintf(stdout, "detections %d\n", num_detections);
-    fprintf(stdout, "dims %ld\n", output.dim());
-    printf("Output tensor shape: [");
-    for (int i = 0; i < output.dim(); i++) {
-        printf("%ld%s", output.size(i), i < output.dim()-1 ? ", " : "");
-    }
-    printf("]\n"); */
-
-    for (int i = 0; i < num_detections; i++) {
-      auto detection = output[0][i];
-      float x = detection[0].item<float>();
-      float y = detection[1].item<float>();
-      float w = detection[2].item<float>();
-      float h = detection[3].item<float>();
-      fprintf(stdout, "RAW %f %f %f %f\n", x, y, w, h);
-
-      int _x = (int) (((x - 0.5 * w) * ratio));
-      int _y = (int) (((y - 0.5 * h) * ratio));
-      int width = (int) ((w * ratio));
-      int height = (int) ((h * ratio));
-
-      fprintf(stdout, "UNSTRETCH %d %d %d %d\n", _x, _y, width, height);
-
-      float obj_conf = detection[4].item<float>();
-      printf("--------------\n");
-      // fprintf(stdout, "confidence %f\n", obj_conf);
-      // auto class_scores = detection.slice(0,5);
-      //
-      //
-      //
-      // for (int i = 0; i < num_detections; i++) {
-      //   printf("\ncls scr %f\n", detection[i].item<float>());
-      // }
-      //
-      // auto max_result = class_scores.max(0);
-      // int class_id = std::get<1>(max_result).item<int>();
-      // float class_conf = std::get<0>(max_result).item<float>();
-      // fprintf(stdout, "final conf %f\nconf %f\n", class_conf * obj_conf,
-      // obj_conf);
-      printf("--------------\n");
-    }
-    return detections;
-  } catch (const std::exception &e) {
-    fprintf(stderr, "fucking torch forward error %s", e.what());
-    throw;
-  }
-}
-
 int main(int argc, char **argv) {
   YoloModel *keyinfo_model = create_yolo_detector(
       "/media/hbdesk/hb_desk_ext/yolov11cpp/models/id_ki_v1.2.pt.torchscript",
-      "/media/hbdesk/hb_desk_ext/yolov11cpp/models/id_ki_classes.txt", 0.7, 0.7,
-      640, 640);
-  cv::Mat image = cv::imread("/media/hbdesk/hb_desk_ext/kh_id_keyinfo/kh_tg_id/472293344_122119262414603145_7291510996292981817_n.jpg");
+      "/media/hbdesk/hb_desk_ext/yolov11cpp/models/id_ki_classes.txt",
+      torch::kCPU, 0.3, 0.3, 640, 640);
+  cv::Mat image =
+      cv::imread("/media/hbdesk/hb_desk_ext/kh_id_keyinfo/kh_tg_id/b9ea8f60-2c5c-4cc1-a193-333874e78deb.jpeg");
   if (image.empty()) {
     fprintf(stderr, "Failed to load image\n");
     return -1;
   };
 
+  // torch::Tensor input_tensor;
+  // preprocess(&image, &input_tensor, INFERENCE_SIZE);
   std::vector<Detection> _a = run_inference(keyinfo_model, &image);
 }
--- src/yolo_model.h
+++ src/yolo_model.h
@@ -2,8 +2,13 @@
 #include <opencv2/opencv.hpp>
 #include <cmath>
 #include <torch/script.h>
+#include <torch/torch.h>
 #define INFERENCE_SIZE 640.0
 #define BATCH_SIZE 1
+
+using torch::indexing::Slice;
+using torch::indexing::None;
+
 
 typedef struct {
   cv::Rect box;
@@ -21,13 +26,106 @@ typedef struct YoloModel {
 } YoloModel;
 
 
+static inline void scale_wh(int original_width, int original_height, 
+                           int* new_width, int* new_height, float* ratio, 
+                           const int target_size = 640) {
+    float r = std::min(
+        static_cast<float>(target_size) / original_width,
+        static_cast<float>(target_size) / original_height
+    );
+    *new_width = std::round(original_width * r);
+    *new_height = std::round(original_height * r);
+    *ratio = r;
+}
+
+torch::Tensor xyxy2xywh(const torch::Tensor& x) {
+    auto y = torch::empty_like(x);
+    y.index_put_({"...", 0}, (x.index({"...", 0}) + x.index({"...", 2})).div(2));
+    y.index_put_({"...", 1}, (x.index({"...", 1}) + x.index({"...", 3})).div(2));
+    y.index_put_({"...", 2}, x.index({"...", 2}) - x.index({"...", 0}));
+    y.index_put_({"...", 3}, x.index({"...", 3}) - x.index({"...", 1}));
+    return y;
+}
+
+torch::Tensor xywh2xyxy(const torch::Tensor& x) {
+    auto y = torch::empty_like(x);
+    auto dw = x.index({"...", 2}).div(2);
+    auto dh = x.index({"...", 3}).div(2);
+    y.index_put_({"...", 0}, x.index({"...", 0}) - dw);
+    y.index_put_({"...", 1}, x.index({"...", 1}) - dh);
+    y.index_put_({"...", 2}, x.index({"...", 0}) + dw);
+    y.index_put_({"...", 3}, x.index({"...", 1}) + dh);
+    return y;
+}
+
+/* preprocess into letterbox format */
+static inline float preprocess(cv::Mat *input_image, 
+                              torch::Tensor *output_tensor, 
+                              const int target_size = 640) {
+    // Get original dimensions
+    int o_width = input_image->cols;
+    int o_height = input_image->rows;
+
+    int new_w, new_h;
+    float ratio;
+    scale_wh(o_width, o_height, &new_w, &new_h, &ratio, target_size);
+
+    cv::Mat canvas = cv::Mat(target_size, target_size, input_image->type(), cv::Scalar(114, 114, 114));
+    
+    cv::Mat resized;
+    cv::resize(*input_image, resized, cv::Size(new_w, new_h), 0, 0, cv::INTER_AREA);
+    
+    int dx = (target_size - new_w) / 2;
+    int dy = (target_size - new_h) / 2;
+
+    cv::Mat roi = canvas(cv::Rect(dx, dy, new_w, new_h));
+    resized.copyTo(roi);
+    
+    // cv::imwrite("./letterbox_debug.png", canvas);
+
+    cv::Mat float_img;
+    canvas.convertTo(float_img, CV_32F, 1.0/255.0);
+    *output_tensor = torch::from_blob(float_img.data, {target_size, target_size, 3}, torch::kFloat32).clone();
+    
+    *output_tensor = output_tensor->permute({2, 0, 1}).contiguous();
+    *output_tensor = output_tensor->unsqueeze(0);
+    std::cout << "input_image tensor shape: [" 
+              << output_tensor->size(0) << ", " 
+              << output_tensor->size(1) << ", " 
+              << output_tensor->size(2) << ", " 
+              << output_tensor->size(3) << "]" << std::endl;
+    return ratio;
+}
+
+
+
+static inline float compute_iou(cv::Rect *box1, cv::Rect *box2) {
+  int x1 = std::max(box1->x, box2->x);
+  int y1 = std::max(box1->y, box2->y);
+  int x2 = std::min(box1->x + box1->width, box2->x + box2->width);
+  int y2 = std::min(box1->y + box1->height, box2->y + box2->height);
+
+  if (x2 <= x1 || y2 <= y1) {
+    return 0.0f;
+  }
+  float intersection = (x2 - x1) * (y2 - y1);
+  float area1 = box1->width * box1->height;
+  float area2 = box2->width * box2->height;
+  float union_area = area1 + area2 - intersection;
+  return intersection / union_area;
+}
+
 static inline YoloModel *create_yolo_detector(const char *model_path,
-                                const char *classes_path, float conf_thresh,
-                                float nms_thresh, int w, int h) {
+                                              const char *classes_path, 
+                                              const torch::Device device,
+                                              float conf_thresh,
+                                              float nms_thresh, 
+                                              int w, int h) {
   YoloModel *detector = new YoloModel();
   try {
     detector->model = torch::jit::load(model_path);
     detector->model.eval();
+    detector->model.to(device, torch::kFloat32);
     std::ifstream class_file(classes_path);
     if (!class_file.is_open()) {
         printf("Failed to open classes file\n");
@@ -53,45 +151,61 @@ static inline YoloModel *create_yolo_detector(const char *model_path,
   return detector;
 }
 
-static inline void scale_wh(int original_width, int original_height, 
-                            int *target_width, int *target_height, float *ratio) {
-  float r = std::min((float) INFERENCE_SIZE / original_width, 
-                     (float) INFERENCE_SIZE / original_height);
+std::vector<Detection> run_inference(YoloModel *detector,
+                                     cv::Mat *input_image) {
 
-  fprintf(stdout, "[debug] scale_wh R %f\n", r);
-  fprintf(stdout, "[debug] mul w %f\n", original_width * r);
-  fprintf(stdout, "[debug] mul h %f\n", original_height * r);
-  *target_width = (int) std::round(original_width * r);
+  // torch::Device device(torch::kCPU);
+  std::vector<Detection> detections;
+  torch::Tensor input_tensor;
 
-  *target_height = (int) std::round(original_height * r);
-  *ratio = r;
-}
-
-static inline void preprocess(cv::Mat *image, torch::Tensor *output, int target_size) {
-  cv::Mat resized;
-  cv::resize(*image, resized, cv::Size(target_size, target_size));
-  cv::cvtColor(resized, resized, cv::COLOR_BGR2RGB);
-  cv::Mat float_img;
-  resized.convertTo(float_img, CV_32F, 1.0 / 255.0);
-  // batch size , w , h , channels
-  *output = torch::from_blob(float_img.data, {BATCH_SIZE, target_size, target_size, 3}, torch::kFloat32).clone(); // dealloc aka not clonign causes a segfault
-  *output = output->permute({0, 3, 1, 2}).contiguous();
-}
-
-
-
-static inline float compute_iou(cv::Rect *box1, cv::Rect *box2) {
-  int x1 = std::max(box1->x, box2->x);
-  int y1 = std::max(box1->y, box2->y);
-  int x2 = std::min(box1->x + box1->width, box2->x + box2->width);
-  int y2 = std::min(box1->y + box1->height, box2->y + box2->height);
-
-  if (x2 <= x1 || y2 <= y1) {
-    return 0.0f;
+  float preproc_ratio = preprocess(input_image, &input_tensor, INFERENCE_SIZE);
+  printf("input_image tensor shape: [");
+  for (int i = 0; i < input_tensor.dim(); i++) {
+    printf("%ld%s", input_tensor.size(i),
+           i < input_tensor.dim() - 1 ? ", " : "");
   }
-  float intersection = (x2 - x1) * (y2 - y1);
-  float area1 = box1->width * box1->height;
-  float area2 = box2->width * box2->height;
-  float union_area = area1 + area2 - intersection;
-  return intersection / union_area;
+  printf("]\n");
+  float conf_thresh = 0.5;
+  float iou_thresh = 0.5;
+  int max_det = 100;
+  try {
+    torch::NoGradGuard no_grad;
+    std::vector<torch::jit::IValue> _inputs;
+    _inputs.push_back(input_tensor);
+    auto output = detector->model.forward(_inputs).toTensor();
+    output = output.to(torch::kCPU);
+
+    /*  [1, 14, 8400]
+     https://github.com/orgs/ultralytics/discussions/17254
+    */
+    int channels = output.size(1);
+    int num_points = output.size(2);
+    
+    std::vector<Detection> detections;
+
+    for (int i=0; i < num_points; i++) {
+      auto objectness = output[0][4][i].item<float>();
+      // float x = output[0][0][i].item<float>();
+      // float y = output[0][1][i].item<float>();
+      // float w = output[0][2][i].item<float>();
+      // float h = output[0][3][i].item<float>();
+      // fprintf(stdout, "%f %f %f %f\n", x,y,w,h);
+
+      if (objectness >= 0.01) {
+        fprintf(stdout, "OBJECTNESS %f\n", objectness);
+        float x = output[0][0][i].item<float>();
+        float y = output[0][1][i].item<float>();
+        float w = output[0][2][i].item<float>();
+        float h = output[0][3][i].item<float>();
+        fprintf(stdout, "%f %f %f %f\n", x,y,w,h);
+      } 
+    }
+    
+    return detections;
+
+  } catch (const std::exception &e) {
+    fprintf(stderr, "fucking torch forward error %s", e.what());
+    throw;
+  }
 }
+
