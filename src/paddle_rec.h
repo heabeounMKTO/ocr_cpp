3f38f38acb3aeef1c2fb3802946b14c23708750d
heabeounMKTO <heabeoun69zin@gmail.com>
2025-03-27 18:07:33 +0700

    fucc2

--- src/paddle_rec.h
+++ src/paddle_rec.h
@@ -1,40 +1,43 @@
-#include <opencv2/core/mat.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <paddle_inference_api.h>
 #include <chrono>
 #include <fstream>
 #include <iostream>
 #include <memory>
-#include <vector>
 #include <numeric>
+#include <opencv2/core.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgproc.hpp>
+#include <paddle_inference_api.h>
+#include <vector>
 
 typedef struct {
-    const std::string model_dir;
-    const bool use_gpu;
-    const int gpu_id;
-    const int gpu_mem;
-    const int cpu_math_library_num_threads;
-    const bool use_mkldnn;
-    const std::string label_path;
-    const bool use_tensorrt;
-    const std::string precision;
-    const int rec_batch_num;
-    const int rec_img_h;
-    const int rec_img_w;
+  const std::string model_dir;
+  const bool use_gpu;
+  const int gpu_id;
+  const int gpu_mem;
+  const int cpu_math_library_num_threads;
+  const bool use_mkldnn;
+  const std::string label_path;
+  const bool use_tensorrt;
+  const std::string precision;
+  const int rec_batch_num;
+  const int rec_img_h;
+  const int rec_img_w;
 } PaddleCharacterRecognizerConfig;
 
-static inline void permute(const cv::Mat &im, float * data) noexcept {
+static inline void permute(const cv::Mat &im, float *data) noexcept {
   int rh = im.rows;
   int rw = im.cols;
   int rc = im.channels();
-  
-  for (int i =0; i < rc; i++) {
+
+  for (int i = 0; i < rc; i++) {
     cv::extractChannel(im, cv::Mat(rh, rw, CV_32FC1, data + i * rh * rw), i);
   }
 }
 
-static inline void rec_resize_input(cv::Mat &img , cv::Mat &resize_img, float wh_ratio, bool use_tensorrt, const std::vector<int> &rec_image_shape) noexcept {
+static inline void
+rec_resize_input(cv::Mat &img, cv::Mat &resize_img, float wh_ratio,
+                 bool use_tensorrt,
+                 const std::vector<int> &rec_image_shape) noexcept {
   int imgC, imgH, imgW;
   imgC = rec_image_shape[0];
   imgH = rec_image_shape[1];
@@ -52,9 +55,12 @@ static inline void rec_resize_input(cv::Mat &img , cv::Mat &resize_img, float wh
   cv::copyMakeBorder(resize_img, resize_img, 0, 0, 0,
                      int(imgW - resize_img.cols), cv::BORDER_CONSTANT,
                      {0, 0, 0});
-} 
+}
 
-static inline void rec_normalize_input(cv::Mat &im, const std::vector<float> &mean, const std::vector<float> &scale, const bool is_scale)  {
+static inline void rec_normalize_input(cv::Mat &im,
+                                       const std::vector<float> &mean,
+                                       const std::vector<float> &scale,
+                                       const bool is_scale) {
   double e = 1.0;
   if (is_scale) {
     e /= 255.0;
@@ -69,64 +75,56 @@ static inline void rec_normalize_input(cv::Mat &im, const std::vector<float> &me
   cv::merge(bgr_channels, im);
 }
 
-
-
 static inline std::vector<std::string>
-read_dict(const std::string& path) noexcept
-{
-    std::vector<std::string> m_vec;
-    std::ifstream in(path);
-    if (in) {
-        for (;;) {
-            std::string line;
-            if (!getline(in, line)) {
-                break;
-            }
-            m_vec.emplace_back(std::move(line));
-        }
-    } else {
-        std::cout << "label file not found! : " << path << "\n"
-                  << std::endl;
-        exit(1);
+read_dict(const std::string &path) noexcept {
+  std::vector<std::string> m_vec;
+  std::ifstream in(path);
+  if (in) {
+    for (;;) {
+      std::string line;
+      if (!getline(in, line)) {
+        break;
+      }
+      m_vec.emplace_back(std::move(line));
     }
-    return m_vec;
+  } else {
+    std::cout << "label file not found! : " << path << "\n" << std::endl;
+    exit(1);
+  }
+  return m_vec;
 }
-
 
 static inline std::shared_ptr<paddle_infer::Predictor> new_paddle_recognizer(
     const std::string model_dir, const bool use_gpu, const int gpu_id,
     const int gpu_mem, const int cpu_math_library_num_threads,
     const bool use_mkldnn, const std::string label_path,
     const bool use_tensorrt, const std::string precision,
-    const int rec_batch_num, const int rec_img_h, const int rec_img_w)
-{
-    paddle_infer::Config config;
-    config.SetModel(model_dir + "/inference.pdmodel",
-        model_dir + "/inference.pdiparams");
-    std::cout << "In PP-OCRv3, default rec_img_h is 48,"
-              << "if you use other model, you should set the param rec_img_h=32"
-              << std::endl;
-    // add gpu suppourt later , its optional
-    config.DisableGpu();
-    config.SetCpuMathLibraryNumThreads(cpu_math_library_num_threads);
-    auto pass_builder = config.pass_builder();
-    pass_builder->DeletePass("matmul_transpose_reshape_fuse_pass");
-    pass_builder->DeletePass("matmul_transpose_reshape_fuse_pass");
-    config.SwitchUseFeedFetchOps(false);
-    // true for multiple input
-    config.SwitchSpecifyInputNames(true);
+    const int rec_batch_num, const int rec_img_h, const int rec_img_w) {
+  paddle_infer::Config config;
+  config.SetModel(model_dir + "/inference.pdmodel",
+                  model_dir + "/inference.pdiparams");
+  std::cout << "In PP-OCRv3, default rec_img_h is 48,"
+            << "if you use other model, you should set the param rec_img_h=32"
+            << std::endl;
+  // add gpu suppourt later , its optional
+  config.DisableGpu();
+  config.SetCpuMathLibraryNumThreads(cpu_math_library_num_threads);
+  auto pass_builder = config.pass_builder();
+  pass_builder->DeletePass("matmul_transpose_reshape_fuse_pass");
+  pass_builder->DeletePass("matmul_transpose_reshape_fuse_pass");
+  config.SwitchUseFeedFetchOps(false);
+  // true for multiple input
+  config.SwitchSpecifyInputNames(true);
 
-    config.SwitchIrOptim(true);
+  config.SwitchIrOptim(true);
 
-    config.EnableMemoryOptim();
-    //   config.DisableGlogInfo();
-    std::shared_ptr<paddle_infer::Predictor> pred = paddle_infer::CreatePredictor(config);
-    return pred;
+  config.EnableMemoryOptim();
+  //   config.DisableGlogInfo();
+  std::shared_ptr<paddle_infer::Predictor> pred =
+      paddle_infer::CreatePredictor(config);
+  return pred;
 }
 
-
-static inline void run_rec_model(std::shared_ptr<paddle_infer::Predictor> &char_rec, const std::vector<cv::Mat> &input_img) {
-
-}
-
-
+static inline void
+run_rec_model(std::shared_ptr<paddle_infer::Predictor> &char_rec,
+              const std::vector<cv::Mat> &input_img) {}
--- src/run_ocr.cpp
+++ src/run_ocr.cpp
@@ -8,45 +8,44 @@
 #include "paddle_rec.h"
 #include "yolo_model.h"
 
-int main(int argc, char* argv[])
-{
-    const std::string model_path = "./models/id_ki_v12.onnx";
-    const std::string image_path = "/home/hbdesk/Pictures/vn_passport/test1.jpg";
-    const std::string labels_path = "./models/id_ki_classes.txt";
-    const std::string save_path = "./test_image.jpeg";
-    YOLO12Detector detector(model_path, labels_path, false);
+int main(int argc, char *argv[]) {
+  const std::string model_path = "./models/id_ki_v12.onnx";
+  const std::string image_path = "/home/hbdesk/Pictures/vn_passport/test1.jpg";
+  const std::string labels_path = "./models/id_ki_classes.txt";
+  const std::string save_path = "./test_image.jpeg";
+  YOLO12Detector detector(model_path, labels_path, false);
 
-    cv::Mat image = cv::imread(image_path);
-    if (image.empty()) {
-        std::cerr << "couldnt open the image!\n";
-        return -1;
-    }
-    auto start = std::chrono::high_resolution_clock::now();
-    std::vector<Detection> results = detector.detect(image);
+  cv::Mat image = cv::imread(image_path);
+  if (image.empty()) {
+    std::cerr << "couldnt open the image!\n";
+    return -1;
+  }
+  auto start = std::chrono::high_resolution_clock::now();
+  std::vector<Detection> results = detector.detect(image);
 
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::high_resolution_clock::now() - start);
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::high_resolution_clock::now() - start);
 
-    std::cout << "Detection completed in: " << duration.count() << " ms"
+  std::cout << "Detection completed in: " << duration.count() << " ms"
+            << std::endl;
+
+  std::shared_ptr<paddle_infer::Predictor> _a = new_paddle_recognizer(
+      "/media/hbdesk/hb_desk_ext/ocr_core/models/"
+      "latin_ppocr_mobile_v2.0_rec_infer",
+      false, 0, 1024, 4, false,
+      "/media/hbdesk/hb_desk_ext/ocr_core/models/kh_dict.txt", false, "f32", 1,
+      112, 112);
+  detector.drawBoundingBox(image, results); // Simple bounding box drawing
+  // detector.drawBoundingBoxMask(image, results); // Uncomment for mask
+  // drawing
+
+  // Save the processed image to the specified directory
+  if (cv::imwrite(save_path, image)) {
+    std::cout << "Processed image saved successfully at: " << save_path
               << std::endl;
-
-    std::shared_ptr<paddle_infer::Predictor> _a = new_paddle_recognizer(
-        "/media/hbdesk/hb_desk_ext/ocr_core/models/"
-        "latin_ppocr_mobile_v2.0_rec_infer",
-        false, 0, 1024, 4, false,
-        "/media/hbdesk/hb_desk_ext/ocr_core/models/kh_dict.txt", false, "f32", 1,
-        112, 112);
-    detector.drawBoundingBox(image, results); // Simple bounding box drawing
-    // detector.drawBoundingBoxMask(image, results); // Uncomment for mask
-    // drawing
-
-    // Save the processed image to the specified directory
-    if (cv::imwrite(save_path, image)) {
-        std::cout << "Processed image saved successfully at: " << save_path
-                  << std::endl;
-    } else {
-        std::cerr << "Error: Could not save the processed image to: " << save_path
-                  << std::endl;
-    }
-    return 0;
+  } else {
+    std::cerr << "Error: Could not save the processed image to: " << save_path
+              << std::endl;
+  }
+  return 0;
 }
